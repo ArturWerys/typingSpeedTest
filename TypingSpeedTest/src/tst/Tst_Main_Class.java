d9a51380d8330c54c187d353fbdb56933421cc2c
roszk <roszk@mateusz-desktop.lan>
2024-03-10 21:31:00 +0100

    Test3

--- TypingSpeedTest/src/tst/Tst_Main_Class.java
+++ TypingSpeedTest/src/tst/Tst_Main_Class.java
@@ -7,6 +7,7 @@ public class Tst_Main_Class {
 	public static void main(String[] args) {
 		System.out.println("Hello MatRos");
 		System.out.println("CipiCipi, ciapa ciapa");
+		System.out.println("Test2");
 
 	}
 
