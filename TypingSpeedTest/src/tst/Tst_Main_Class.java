51f14a863c005e54e08ebaa2aa7af9e7874da05b
Artur Werys <arturwerys@gmail.com>
2024-03-17 21:43:26 +0100

    Pierwsza wersja GUI

--- TypingSpeedTest/src/tst/Tst_Main_Class.java
+++ TypingSpeedTest/src/tst/Tst_Main_Class.java
@@ -1,48 +1,51 @@
 package tst;
 
 import java.awt.BorderLayout;
-
+import java.awt.Dimension;
+import java.awt.HeadlessException;
 import java.util.Vector;
 
 import javax.swing.*;
 
 public class Tst_Main_Class {
-
 	
-
 	public static void main(String[] args) {
 
+		TestWindowPanels frame = new TestWindowPanels();
+		
 		// Vector
 		
-				Vector<String> textToWrite = new Vector<String>();
-				Vector<String> textToVector = new Vector<String>();
+//		Vector<String> textToWrite = new Vector<String>();
+//		Vector<String> textToVector = new Vector<String>();
+//		
+//		textToWrite.add("Test Test Test Test");
+//		
+//		String a = textToWrite.elementAt(0);
+//	
+//		// Window
+//		
+//		CloseableFrame frame = new CloseableFrame();
+//	    
+//		frame.setSize(windowWidth, windowHeight);
+//		
+//		
+//		JPanel jpanel_1 = new JPanel();
+//		
+//	    JLabel centralLabel = new JLabel(a);
+//	    jpanel_1.add(centralLabel);
+//	    
+//	    frame.add(jpanel_1, BorderLayout.CENTER);
+//		
+//		
+//		// TestField
+//		
+//		JTextField testField = new JTextField();
+//		testField.setPreferredSize(new Dimension(200, 50));
+//		
+//		frame.add(testField,BorderLayout.PAGE_END);
+//		
+		frame.setVisible(true);
 				
-				
-				textToWrite.add("Test TEst TEst TEst ");
-				
-				String a = textToWrite.elementAt(0);
-
-				// Window
-				
-				CloseableFrame frame = new CloseableFrame();
-		        
-				frame.setSize(800, 600);
-				
-				JPanel jpanel_1 = new JPanel();
-				
-		        JLabel centralLabel = new JLabel(a);
-		        jpanel_1.add(centralLabel);
-		        
-		        frame.add(jpanel_1, BorderLayout.CENTER);
-				
-				
-				// TextField
-				
-				JTextField testField = new JTextField();
-				
-				frame.add(testField,BorderLayout.PAGE_END);
-				
-				frame.setVisible(true);
 
 	}
 
